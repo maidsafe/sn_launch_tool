64cff5b222cae5617f4774c1bc4ab496bfa940f4
Gabriel Viganotti <gabrielviganotti@gmail.com>
2020-02-17 12:30:47 -0300

    feat/api: allow to run tool by exposing a public run_with API

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,11 @@ const SAFE_VAULT_EXECUTABLE: &str = "safe_vault.exe";
 /// Currently, this tool runs vaults on localhost (since that's the default if no IP address is given to the vaults)
 #[derive(StructOpt, Debug)]
 #[structopt(name = "safe-nlt")]
-struct Opt {
+struct CmdArgs {
+    /// Vebosity level for this tool
+    #[structopt(short = "v", long, parse(from_occurrences))]
+    verbosity: u8,
+
     /// Path where to locate safe_vault/safe_vault.exe binary. The SAFE_VAULT_PATH env var can be also used to set the path
     #[structopt(short = "p", long, env = "SAFE_VAULT_PATH")]
     vault_path: Option<PathBuf>,
@@ -46,25 +50,44 @@ struct Opt {
     num_vaults: u8,
 
     /// Vebosity level for vaults logs
-    #[structopt(short = "v", parse(from_occurrences))]
-    vault_verbosity: u8,
+    #[structopt(short = "y", long, parse(from_occurrences))]
+    vaults_verbosity: u8,
 }
 
 pub fn run() -> Result<(), String> {
-    let opt = Opt::from_args();
-    let vault_bin_path = get_vault_bin_path(opt.vault_path)?;
-    debug!(
-        "Launching with safe_vault executable from: {}",
+    run_with(None)
+}
+
+pub fn run_with(cmd_args: Option<&[&str]>) -> Result<(), String> {
+    // Let's first get all the arguments passed in, either as function's args, or CLI args
+    let args = match cmd_args {
+        None => CmdArgs::from_args(),
+        Some(cmd_args) => CmdArgs::from_iter_safe(cmd_args).map_err(|err| err.to_string())?,
+    };
+
+    let vault_bin_path = get_vault_bin_path(args.vault_path)?;
+    let msg = format!(
+        "Launching with vault executable from: {}",
         vault_bin_path.display()
     );
+    if args.verbosity > 0 {
+        println!("{}", msg);
+    }
+    debug!("{}", msg);
+
+    let msg = format!("Network size: {} vaults", args.num_vaults);
+    if args.verbosity > 0 {
+        println!("{}", msg);
+    }
+    debug!("{}", msg);
 
     // TODO: read genesis IP and port number from genesis vault stdout output
     let genesis_port_number = "40000";
     let mut common_args: Vec<&str> = vec![];
 
     let mut verbosity = String::from("-");
-    if opt.vault_verbosity > 0 {
-        for _ in 0..opt.vault_verbosity {
+    if args.vaults_verbosity > 0 {
+        for _ in 0..args.vaults_verbosity {
             verbosity.push('v');
         }
         common_args.push(&verbosity);
@@ -73,7 +96,7 @@ pub fn run() -> Result<(), String> {
     // Construct genesis vault's command arguments
     let mut genesis_vault_args = common_args.clone();
     genesis_vault_args.push("--first");
-    let genesis_vault_dir = &opt
+    let genesis_vault_dir = &args
         .vaults_dir
         .join("safe-vault-genesis")
         .display()
@@ -84,18 +107,22 @@ pub fn run() -> Result<(), String> {
     genesis_vault_args.push(genesis_port_number);
 
     // Let's launch genesis vault now
-    debug!("Launching genesis vault...");
-    run_vault_cmd(&vault_bin_path, &genesis_vault_args)?;
+    let msg = "Launching genesis vault (#1)...";
+    if args.verbosity > 0 {
+        println!("{}", msg);
+    }
+    debug!("{}", msg);
+    run_vault_cmd(&vault_bin_path, &genesis_vault_args, args.verbosity)?;
 
     // We can now run the rest of the vaults
-    for i in 1..opt.num_vaults {
+    for i in 2..args.num_vaults + 1 {
         // We wait for a few secs before launching each new vault
-        let interval_duration = time::Duration::from_secs(opt.interval);
+        let interval_duration = time::Duration::from_secs(args.interval);
         thread::sleep(interval_duration);
 
         // Construct current vault's command arguments
         let mut current_vault_args = common_args.clone();
-        let vault_dir = &opt
+        let vault_dir = &args
             .vaults_dir
             .join(&format!("safe-vault-{}", i))
             .display()
@@ -107,8 +134,12 @@ pub fn run() -> Result<(), String> {
         let contacts = format!("[\"127.0.0.1:{}\"]", genesis_port_number);
         current_vault_args.push(&contacts);
 
-        debug!("Launching vault #{}...", i);
-        run_vault_cmd(&vault_bin_path, &current_vault_args)?;
+        let msg = format!("Launching vault #{}...", i);
+        if args.verbosity > 0 {
+            println!("{}", msg);
+        }
+        debug!("{}", msg);
+        run_vault_cmd(&vault_bin_path, &current_vault_args, args.verbosity)?;
     }
 
     println!("Done!");
@@ -132,9 +163,13 @@ fn get_vault_bin_path(vault_path: Option<PathBuf>) -> Result<PathBuf, String> {
     }
 }
 
-fn run_vault_cmd(vault_path: &PathBuf, args: &[&str]) -> Result<(), String> {
+fn run_vault_cmd(vault_path: &PathBuf, args: &[&str], verbosity: u8) -> Result<(), String> {
     let path_str = vault_path.display().to_string();
-    debug!("Running '{}' with args {:?} ...", path_str, args);
+    let msg = format!("Running '{}' with args {:?} ...", path_str, args);
+    if verbosity > 1 {
+        println!("{}", msg);
+    }
+    debug!("{}", msg);
 
     let _child = Command::new(&path_str)
         .args(args)
