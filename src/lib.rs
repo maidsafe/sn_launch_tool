50392e0ed5df3798039d3cb84d06dcb022dc2cf2
oetyng <oetyng@gmail.com>
2021-04-06 10:34:26 +0200

    feat(multi_section): join random section

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,13 @@
 // Software.
 
 use log::debug;
+use rand::Rng;
 use std::{
     collections::HashSet,
     env,
-    fs::File,
+    fs::{self, File},
     io::{self, BufReader, Write},
-    net::SocketAddr,
+    net::{IpAddr, Ipv4Addr, SocketAddr},
     path::{Path, PathBuf},
     process::{Command, Stdio},
     thread,
@@ -27,8 +28,11 @@ const SN_NODE_EXECUTABLE: &str = "sn_node";
 #[cfg(target_os = "windows")]
 const SN_NODE_EXECUTABLE: &str = "sn_node.exe";
 
-// Relative path from $HOME where to read the genesis node connection information from
-const GENESIS_CONN_INFO_FILEPATH: &str = ".safe/node/node_connection_info.config";
+// Empty prefix file name
+const DEFAULT_CONN_INFO_FILENAME: &str = "_node_connection_info.config";
+
+// Relative path from $HOME where to read the connection information from
+const BOOTSTRAP_CONN_INFO_PATH: &str = ".safe/node/bootstrap";
 
 const DEFAULT_RUST_LOG: &str = "sn_node=debug";
 
@@ -250,9 +254,6 @@ pub fn run_with(cmd_args: Option<&[&str]>) -> Result<(), String> {
     let interval_duration = Duration::from_secs(args.interval);
     thread::sleep(interval_duration);
 
-    // Fetch node_conn_info from $HOME/.safe/node/node_connection_info.config.
-    let genesis_contact_info = read_genesis_conn_info(args.verbosity)?;
-
     if args.verbosity > 0 {
         println!(
             "Common node args for launching the network: {:?}",
@@ -262,6 +263,9 @@ pub fn run_with(cmd_args: Option<&[&str]>) -> Result<(), String> {
 
     // We can now run the rest of the nodes
     for i in 2..args.num_nodes + 1 {
+        // Fetch contact info from $HOME/.safe/node/
+        let contact_info = read_bootstrap_info(args.verbosity)?;
+
         // Construct current node's command arguments
         let node_dir = &args
             .nodes_dir
@@ -270,7 +274,7 @@ pub fn run_with(cmd_args: Option<&[&str]>) -> Result<(), String> {
             .to_string();
 
         let current_node_args =
-            build_node_args(common_args.clone(), &node_dir, Some(&genesis_contact_info));
+            build_node_args(common_args.clone(), &node_dir, Some(&contact_info));
 
         let msg = format!("Launching node #{}...", i);
         if args.verbosity > 0 {
@@ -382,26 +386,53 @@ fn run_node_cmd(
     Ok(())
 }
 
-fn read_genesis_conn_info(verbosity: u8) -> Result<String, String> {
+fn read_bootstrap_info(verbosity: u8) -> Result<String, String> {
     let home_dir = dirs_next::home_dir().ok_or("Home directory not found")?;
-    let conn_info_path = home_dir.join(GENESIS_CONN_INFO_FILEPATH);
+    let conn_info_path = home_dir.join(BOOTSTRAP_CONN_INFO_PATH);
+    #[allow(unused_assignments)]
+    let mut path_for_display = conn_info_path.clone();
+    let sections = fs::read_dir(conn_info_path)
+        .map_err(|err| format!("Failed to read dir {}", err))?
+        .map(|res| res.map(|e| (e.path(), e.file_name())))
+        .collect::<Result<Vec<_>, std::io::Error>>()
+        .map_err(|err| format!("Failed to collect sections {}", err))?;
 
-    let file = File::open(&conn_info_path).map_err(|err| {
-        format!(
-            "Failed to open node connection information file at '{}': {}",
-            conn_info_path.display(),
-            err
-        )
-    })?;
-    let reader = BufReader::new(file);
-    let hard_coded_contacts: HashSet<SocketAddr> =
-        serde_json::from_reader(reader).map_err(|err| {
-            format!(
-                "Failed to parse content of node connection information file at '{}': {}",
-                conn_info_path.display(),
-                err
-            )
-        })?;
+    let localhost = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+    let mut hard_coded_contacts = HashSet::new();
+    if sections.is_empty() {
+        path_for_display = home_dir
+            .join(BOOTSTRAP_CONN_INFO_PATH)
+            .join(DEFAULT_CONN_INFO_FILENAME);
+        hard_coded_contacts.insert(SocketAddr::new(localhost, 12000));
+    } else {
+        let mut rng = rand::thread_rng();
+        let index = rng.gen_range(0, sections.len());
+        if let Some((_, section_filename)) = sections.get(index) {
+            path_for_display = home_dir
+                .join(BOOTSTRAP_CONN_INFO_PATH)
+                .join(section_filename);
+            let file = File::open(&path_for_display).map_err(|err| {
+                format!(
+                    "Failed to open node connection information file at '{}': {}",
+                    path_for_display.display(),
+                    err
+                )
+            })?;
+            let reader = BufReader::new(file);
+            hard_coded_contacts = serde_json::from_reader(reader).map_err(|err| {
+                format!(
+                    "Failed to parse content of node connection information file at '{}': {}",
+                    path_for_display.display(),
+                    err
+                )
+            })?;
+        } else {
+            path_for_display = home_dir
+                .join(BOOTSTRAP_CONN_INFO_PATH)
+                .join(DEFAULT_CONN_INFO_FILENAME);
+            hard_coded_contacts.insert(SocketAddr::new(localhost, 12000));
+        }
+    };
 
     let contacts: Vec<String> = hard_coded_contacts.iter().map(|c| c.to_string()).collect();
 
@@ -412,13 +443,13 @@ fn read_genesis_conn_info(verbosity: u8) -> Result<String, String> {
         )
     })?;
 
-    let msg = format!("Genesis node contact info: {}", conn_info_str);
+    let msg = format!("Connection info: {}", conn_info_str);
     if verbosity > 0 {
-        println!("Connection info directory: {}", conn_info_path.display());
+        println!("Connection info directory: {}", path_for_display.display());
         println!("{}", msg);
     }
     debug!("{}", msg);
-    debug!("Connection info directory: {}", conn_info_path.display());
+    debug!("Connection info directory: {}", path_for_display.display());
 
     Ok(conn_info_str)
 }
