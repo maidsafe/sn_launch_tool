c187566ef2ee7ad3ab31bd365754a9644b8351c6
Chris Connelly <chris.connelly@maidsafe.net>
2021-09-01 21:12:43 +0100

    refactor: Move 'run' fn body into their respective structs
    The associated `run` functions now contain the whole logic, rather than
    delegating to a module-level function.

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ use std::{
     fs::File,
     io::{self, BufReader},
     net::SocketAddr,
-    path::{Path, PathBuf},
+    path::PathBuf,
     thread,
     time::Duration,
 };
@@ -82,18 +82,83 @@ pub struct Launch {
 impl Launch {
     /// Launch a network with these arguments.
     pub fn run(&self) -> Result<()> {
-        launch(self)
-    }
+        let mut node_cmd = self.common.node_cmd()?;
+        node_cmd.print_version()?;
 
-    fn node_cmd(&self) -> Result<NodeCmd<'_>> {
-        let mut cmd = self.common.node_cmd()?;
+        node_cmd.push_arg("--idle-timeout-msec");
+        node_cmd.push_arg(self.idle_timeout_msec.to_string());
+        node_cmd.push_arg("--keep-alive-interval-msec");
+        node_cmd.push_arg(self.keep_alive_interval_msec.to_string());
 
-        cmd.push_arg("--idle-timeout-msec");
-        cmd.push_arg(self.idle_timeout_msec.to_string());
-        cmd.push_arg("--keep-alive-interval-msec");
-        cmd.push_arg(self.keep_alive_interval_msec.to_string());
+        debug!("Network size: {} nodes", self.num_nodes);
 
-        Ok(cmd)
+        let interval_duration = Duration::from_secs(self.interval);
+
+        if !self.add_nodes_to_existing_network {
+            // Set genesis node's command arguments
+            let mut genesis_cmd = node_cmd.clone();
+            genesis_cmd.push_arg("--first");
+            if let Some(ip) = &self.ip {
+                genesis_cmd.push_arg(format!("{}:0", ip));
+            } else {
+                genesis_cmd.push_arg("127.0.0.1:0");
+            }
+
+            // Let's launch genesis node now
+            debug!("Launching genesis node (#1)...");
+            genesis_cmd.run(self.nodes_dir.join("sn-node-genesis"), &[])?;
+
+            thread::sleep(interval_duration);
+        }
+
+        // Fetch node_conn_info from $HOME/.safe/node/node_connection_info.config.
+        let genesis_contact_info = read_genesis_conn_info()?;
+
+        debug!(
+            "Common node args for launching the network: {:?}",
+            node_cmd.args()
+        );
+
+        let paths =
+            fs::read_dir(&self.nodes_dir).wrap_err("Could not read existing testnet log dir")?;
+
+        let existing_nodes_count = paths
+            .collect::<Result<Vec<_>, io::Error>>()
+            .wrap_err("Error collecting testnet log dir")?
+            .len();
+
+        info!("{:?} existing nodes found", existing_nodes_count);
+
+        if existing_nodes_count == 0 {
+            return Err(eyre!("A genesis node could not be found."));
+        }
+
+        let end: usize = if self.add_nodes_to_existing_network {
+            existing_nodes_count + self.num_nodes
+        } else {
+            self.num_nodes
+        };
+
+        // We can now run the rest of the nodes
+        for i in existing_nodes_count..end {
+            let this_node = i + 1;
+
+            if self.add_nodes_to_existing_network {
+                debug!("Adding node #{}...", this_node)
+            } else {
+                debug!("Launching node #{}...", this_node)
+            };
+            node_cmd.run(
+                self.nodes_dir.join(format!("sn-node-{}", this_node)),
+                &genesis_contact_info,
+            )?;
+
+            // We wait for a few secs before launching each new node
+            thread::sleep(interval_duration);
+        }
+
+        info!("Done!");
+        Ok(())
     }
 }
 
@@ -131,32 +196,48 @@ pub struct Join {
 impl Join {
     /// Join a network with these arguments.
     pub fn run(&self) -> Result<()> {
-        join(self)
-    }
-
-    fn node_cmd(&self) -> Result<NodeCmd<'_>> {
-        let mut cmd = self.common.node_cmd()?;
+        let mut node_cmd = self.common.node_cmd()?;
+        node_cmd.print_version()?;
 
         if let Some(max_capacity) = self.max_capacity {
-            cmd.push_arg("--max-capacity");
-            cmd.push_arg(max_capacity.to_string());
+            node_cmd.push_arg("--max-capacity");
+            node_cmd.push_arg(max_capacity.to_string());
         }
 
         if let Some(local_addr) = self.local_addr {
-            cmd.push_arg("--local-addr");
-            cmd.push_arg(local_addr.to_string());
+            node_cmd.push_arg("--local-addr");
+            node_cmd.push_arg(local_addr.to_string());
         }
 
         if let Some(public_addr) = self.public_addr {
-            cmd.push_arg("--public-addr");
-            cmd.push_arg(public_addr.to_string());
+            node_cmd.push_arg("--public-addr");
+            node_cmd.push_arg(public_addr.to_string());
         }
 
         if self.clear_data {
-            cmd.push_arg("--clear-data");
+            node_cmd.push_arg("--clear-data");
         }
 
-        Ok(cmd)
+        if self.hard_coded_contacts.is_empty() {
+            debug!("Failed to start a node. No contacts nodes provided.");
+            return Ok(());
+        }
+
+        debug!(
+            "Node to be started with contact(s): {:?}",
+            self.hard_coded_contacts
+        );
+
+        debug!("Launching node...");
+        node_cmd.run(&self.nodes_dir, &self.hard_coded_contacts)?;
+
+        debug!(
+            "Node logs are being stored at: {}/sn_node.log<DATETIME>",
+            self.nodes_dir.display()
+        );
+        debug!("(Note that log files are rotated hourly, and subsequent files will be named sn_node.log<NEW DATE TINE>.");
+
+        Ok(())
     }
 }
 
@@ -177,7 +258,17 @@ struct CommonArgs {
 
 impl CommonArgs {
     fn node_cmd(&self) -> Result<NodeCmd> {
-        let mut cmd = NodeCmd::new(self.node_path()?);
+        let mut cmd = match self.node_path.as_deref() {
+            Some(p) => NodeCmd::new(p),
+            None => {
+                let mut path =
+                    dirs_next::home_dir().ok_or_else(|| eyre!("Home directory not found"))?;
+
+                path.push(".safe/node");
+                path.push(SN_NODE_EXECUTABLE);
+                NodeCmd::new(path)
+            }
+        };
 
         let rust_log = self.rust_log();
         info!("Using RUST_LOG '{}'", rust_log);
@@ -192,20 +283,6 @@ impl CommonArgs {
         Ok(cmd)
     }
 
-    fn node_path(&self) -> Result<Cow<'_, Path>> {
-        match self.node_path.as_deref() {
-            Some(p) => Ok(p.into()),
-            None => {
-                let mut path =
-                    dirs_next::home_dir().ok_or_else(|| eyre!("Home directory not found"))?;
-
-                path.push(".safe/node");
-                path.push(SN_NODE_EXECUTABLE);
-                Ok(path.into())
-            }
-        }
-    }
-
     fn rust_log(&self) -> Cow<'_, str> {
         match self.rust_log.as_deref() {
             Some(rust_log_flag) => rust_log_flag.into(),
@@ -215,107 +292,6 @@ impl CommonArgs {
             },
         }
     }
-}
-
-fn launch(args: &Launch) -> Result<()> {
-    let node_cmd = args.node_cmd()?;
-    node_cmd.print_version()?;
-
-    debug!("Network size: {} nodes", args.num_nodes);
-
-    let interval_duration = Duration::from_secs(args.interval);
-
-    if !args.add_nodes_to_existing_network {
-        // Set genesis node's command arguments
-        let mut genesis_cmd = node_cmd.clone();
-        genesis_cmd.push_arg("--first");
-        if let Some(ip) = &args.ip {
-            genesis_cmd.push_arg(format!("{}:0", ip));
-        } else {
-            genesis_cmd.push_arg("127.0.0.1:0");
-        }
-
-        // Let's launch genesis node now
-        debug!("Launching genesis node (#1)...");
-        genesis_cmd.run(args.nodes_dir.join("sn-node-genesis"), &[])?;
-
-        thread::sleep(interval_duration);
-    }
-
-    // Fetch node_conn_info from $HOME/.safe/node/node_connection_info.config.
-    let genesis_contact_info = read_genesis_conn_info()?;
-
-    debug!(
-        "Common node args for launching the network: {:?}",
-        node_cmd.args()
-    );
-
-    let paths =
-        fs::read_dir(&args.nodes_dir).wrap_err("Could not read existing testnet log dir")?;
-
-    let existing_nodes_count = paths
-        .collect::<Result<Vec<_>, io::Error>>()
-        .wrap_err("Error collecting testnet log dir")?
-        .len();
-
-    info!("{:?} existing nodes found", existing_nodes_count);
-
-    if existing_nodes_count == 0 {
-        return Err(eyre!("A genesis node could not be found."));
-    }
-
-    let end: usize = if args.add_nodes_to_existing_network {
-        existing_nodes_count + args.num_nodes
-    } else {
-        args.num_nodes
-    };
-
-    // We can now run the rest of the nodes
-    for i in existing_nodes_count..end {
-        let this_node = i + 1;
-
-        if args.add_nodes_to_existing_network {
-            debug!("Adding node #{}...", this_node)
-        } else {
-            debug!("Launching node #{}...", this_node)
-        };
-        node_cmd.run(
-            args.nodes_dir.join(format!("sn-node-{}", this_node)),
-            &genesis_contact_info,
-        )?;
-
-        // We wait for a few secs before launching each new node
-        thread::sleep(interval_duration);
-    }
-
-    info!("Done!");
-    Ok(())
-}
-
-fn join(args: &Join) -> Result<()> {
-    let node_cmd = args.node_cmd()?;
-    node_cmd.print_version()?;
-
-    if args.hard_coded_contacts.is_empty() {
-        debug!("Failed to start a node. No contacts nodes provided.");
-        return Ok(());
-    }
-
-    debug!(
-        "Node to be started with contact(s): {:?}",
-        args.hard_coded_contacts
-    );
-
-    debug!("Launching node...");
-    node_cmd.run(&args.nodes_dir, &args.hard_coded_contacts)?;
-
-    debug!(
-        "Node logs are being stored at: {}/sn_node.log<DATETIME>",
-        args.nodes_dir.display()
-    );
-    debug!("(Note that log files are rotated hourly, and subsequent files will be named sn_node.log<NEW DATE TINE>.");
-
-    Ok(())
 }
 
 fn read_genesis_conn_info() -> Result<Vec<SocketAddr>> {
